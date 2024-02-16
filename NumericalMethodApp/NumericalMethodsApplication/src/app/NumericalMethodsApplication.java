20e832ac25635c5f54ff60a1a4def9abb5dd89f0
RomeroIsHere <guillermoromecu@gmail.com>
2024-02-15 19:59:23 -0600

    Started the Repository

---
+++ NumericalMethodApp/NumericalMethodsApplication/src/app/NumericalMethodsApplication.java
@@ -0,0 +1,14 @@
+package app;
+
+import app.numericalMethods.*;
+
+//import numericalMethods.Bisect;
+
+public class NumericalMethodsApplication {
+	public static void main(String[] args) {
+		System.out.println("Felt");
+		Bisect bis = new Bisect();
+		bis.toString();
+		
+	}
+}
---
+++ NumericalMethodApp/NumericalMethodsApplication/src/app/numericalMethods/Secant.java
@@ -0,0 +1,5 @@
+package app.numericalMethods;
+
+public class Secant {
+
+}
