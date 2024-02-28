0e51efb6d6b7ab18d42ed1f9305ef53b291c6384
RomeroIsHere <guillermoromecu@gmail.com>
2024-02-27 23:56:27 -0600

    Removed Testing code in Main method

--- NumericalMethodApp/NumericalMethodsApplication/src/app/NumericalMethodsApplication.java
+++ NumericalMethodApp/NumericalMethodsApplication/src/app/NumericalMethodsApplication.java
@@ -12,12 +12,5 @@ public class NumericalMethodsApplication {
 		
 		String testExpression="x^3",testVariable="x";
 		Function fun=new Function(testVariable,testExpression);
-		String splitterString="";
-		String[] bashString= "x^3+x-16".split(splitterString);
-		System.out.println(Arrays.toString(bashString));
-		MathTokenizer mTokenizer=new MathTokenizer("x^3+x-16");
-		while (mTokenizer.hasNext()) {
-			System.out.println(mTokenizer.next());
-		}
 	}
 }
