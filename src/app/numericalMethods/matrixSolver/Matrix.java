bbd67d3e267958a1773ed6230b96b61606b599e7
RomeroIsHere <guillermoromecu@gmail.com>
2024-03-08 08:59:48 -0600

    Matrix Object
    Added Sumation and Substraction Methods for Rows and Columns in Matrix
    Object

--- src/app/numericalMethods/matrixSolver/Matrix.java
+++ src/app/numericalMethods/matrixSolver/Matrix.java
@@ -11,10 +11,9 @@ public class Matrix {
 				matrixArray=new double[rows][columns];
 			}
 		}
-		
-		
-		
-		// TODO Auto-generated constructor stub
+	}
+	public Matrix(double[][] arr) {
+		this.matrixArray=arr.clone();
 	}
 	public void switchRows(int firstRow, int secondRow) {
 		
@@ -26,18 +25,40 @@ public class Matrix {
 	}
 	public void switchColumns(int firstColumn, int secondColumn) {
 		if (firstColumn>=0&&firstColumn<=columns&&secondColumn>=0&&secondColumn<=columns) {
-			//TODO: Add Column switcher	
+			for (int row = 0; row < rows; row++) {
+				double temp=matrixArray[row][firstColumn];
+				matrixArray[row][firstColumn]=matrixArray[row][secondColumn];
+				matrixArray[row][secondColumn]=temp;
+			}
 		}
 		
 	}
-	public void sumRows(int firstRow, int secondRow) {
-		if (firstRow>=0&&firstRow<=rows&&secondRow>=0&&secondRow<=rows) {
-			//TODO: add Sumation functionality
+	public void sumRows(int sumatorRow, int sumandRow) {
+		if (sumatorRow>=0&&sumatorRow<=rows&&sumandRow>=0&&sumandRow<=rows) {
+			for (int column = 0; column < columns; column++) {
+				matrixArray[sumandRow][column]+=matrixArray[sumatorRow][column];
+			}
 		}
 	}
-	public void substractRows(int firstRow, int secondRow) {
-		if (firstRow>=0&&firstRow<=rows&&secondRow>=0&&secondRow<=rows) {
-			//TODO: add substraction functionality
+	public void substractRows(int sumatorRow, int sumandRow) {
+		if (sumatorRow>=0&&sumatorRow<=rows&&sumandRow>=0&&sumandRow<=rows) {
+			for (int column = 0; column < columns; column++) {
+				matrixArray[sumandRow][column]-=matrixArray[sumatorRow][column];
+			}
+		}
+	}
+	public void sumColumns(int sumatorColumn, int sumandColumn) {
+		if (sumatorColumn>=0&&sumatorColumn<=rows&&sumandColumn>=0&&sumandColumn<=rows) {
+			for (int row = 0; row < rows; row++) {
+				matrixArray[row][sumandColumn]+=matrixArray[row][sumatorColumn];
+			}
+		}
+	}
+	public void substractColumns(int sumatorColumn, int sumandColumn) {
+		if (sumatorColumn>=0&&sumatorColumn<=rows&&sumandColumn>=0&&sumandColumn<=rows) {
+			for (int row = 0; row < rows; row++) {
+				matrixArray[row][sumandColumn]-=matrixArray[row][sumatorColumn];
+			}
 		}
 	}
 }
